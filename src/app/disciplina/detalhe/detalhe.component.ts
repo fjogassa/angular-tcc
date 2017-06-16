349a0a963b133e8d21a513090ac364f4a41e0eab
fabio <fabio@HOME-PCL.br>
2017-06-16 19:57:00 -0300

    Ajuste na tela de disciplina, nao estava mostrando os alunos da disciplina

--- src/app/disciplina/detalhe/detalhe.component.ts
+++ src/app/disciplina/detalhe/detalhe.component.ts
@@ -13,9 +13,7 @@ import { SuperDetalhes } from '../../comum/superdetalhes.component';
 export class DetalheDisciplinaComponent extends SuperDetalhes implements OnInit {
 
   listaAlunos = [];
-  selecionado = {itens: []};
-  listaSelecionado = {alunos: []};  
-  colunas: any[];
+  selecionado = {alunos: []};  
   aluno;
 
   constructor(service: DisciplinaService, router: Router, route: ActivatedRoute, 
@@ -23,57 +21,30 @@ export class DetalheDisciplinaComponent extends SuperDetalhes implements OnInit 
       super(service, router, route);
   }
 
-  inserirAluno(){
-    console.log('DetalheComponent/Inserir Aluno: ' + this.selecionado.itens, this.aluno);
-     
-    if (!this.selecionado.itens) {
-      console.log('DetalheComponent/********1******: ' + this.selecionado.itens);
-      this.selecionado.itens = [];
-    }
-    console.log('DetalheComponent/********2******: ' + this.selecionado.itens);
-    this.selecionado.itens.push({aluno:this.aluno, quantidade:1});
-    console.log('DetalheComponent/********3******: ' + this.selecionado.itens);
+  inserirAluno(){    
+    if (!this.selecionado.alunos) {
+      this.selecionado.alunos = [];
+    }    
+    this.selecionado.alunos.push(this.aluno);    
+    this.selecionado.alunos = [...this.selecionado.alunos];
     this.aluno = null;
-
-    console.log('DetalheComponent/Inserir Aluno/Final: ' + this.selecionado.itens);
   }
 
   ngOnInit() {
     super.ngOnInit();
     this.atualizaAlunos();
-
-    this.colunas = [
-            {field: 'nome', header: 'Nome'},
-            {field: 'quantidade', header: 'Quantidade'}
-        ];
   }
 
-  atualizaAlunos() {
-    console.log('DetalheComponent/Atualizar Aluno: ' + this.selecionado.itens, this.aluno);
+  atualizaAlunos() {    
     this.alunoService.getAll().then(response => {
       this.listaAlunos = response.values;
     });
   }
 
-  filtrarAlunos(event) {
-    console.log('DetalheComponent/Filtar Aluno: ' + this.selecionado.itens, this.aluno);
+  filtrarAlunos(event) {    
     let query = event.query;
     this.alunoService.filtra(query).then(response => {
       this.listaAlunos = response.values;
     });
   }
-
-  salvar() {
-    console.log('DetalheComponent/Salvar/Inicio: ' + this.selecionado);
-
-    for (let i = 0; i < this.selecionado.itens.length; i++) {
-      this.listaSelecionado.alunos = this.selecionado.itens;
-    }
-
-    console.log('DetalheComponent/Salvar/Fim: ' + this.selecionado);
-    
-    this.service.add(this.listaSelecionado)
-      .then((data) => this.router.navigate(['../..'], { relativeTo: this.route }));
-  }  
-
 }
